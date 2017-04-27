107626b0e629daafafd2c58c63f129deffc6515d
paramitamirza <paramita135@gmail.com>
2017-04-28 01:53:15 +0200

    Update embedding extractor script

--- extractVectors.py
+++ extractVectors.py
@@ -12,17 +12,19 @@ out_file = token_file.replace("tokens", "embedding-word2vec-300")
 token_fin = open(token_file, "r")
 embed_fout = open(out_file, "w")
 
-model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+#model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
 for pair in token_fin.readlines():
     cols = pair.strip().split(",")
-    if cols[0].lower() in model:
-        vec1 = ','.join(['%.6f' % num for num in model[cols[0].lower()]])
+    token1 = cols[0].replace("\"", "")
+    token2 = cols[1].replace("\"", "")
+    if token1.lower() in model:
+        vec1 = ','.join(['%.6f' % num for num in model[token1.lower()]])
     else:
         vec1 = ','.join(['%.6f' % num for num in np.zeros(300)])
-    if cols[1] != "O":
-        if cols[1].lower() in model:
-            vec2 = ','.join(['%.6f' % num for num in model[cols[1].lower()]])
+    if token2 != "O":
+        if token2.lower() in model:
+            vec2 = ','.join(['%.6f' % num for num in model[token2.lower()]])
         else:
             vec2 = ','.join(['%.6f' % num for num in np.zeros(300)])
     embed_fout.write(vec1 + ',' + vec2 + ',' + label + '\n')
